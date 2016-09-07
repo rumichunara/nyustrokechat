3f596b592f3a57f01f37a09f50ae0de757ffd34a
Matías Barrios <matiasbarrios@gmail.com>
2016-09-07 13:32:09 -0300

    Adding Forgot Password Recovery. Editing the user profile (name and image).

--- app/nyu.chat.js
+++ app/nyu.chat.js
@@ -5,6 +5,15 @@ angular
 mainController.$inject = ['$rootScope', '$window', '$scope'];
 function mainController($rootScope, $window, $scope) {
   
+  // General stuff
+  $scope.get_url_of_image = function (url, callback) {
+    if (url.startsWith('gs://'))
+      firebase.storage().refFromURL(url).getMetadata().then(function(metadata) {
+        callback(metadata.downloadURLs[0]);
+        $scope.$apply();
+      });
+  }
+  
   // Authentication stuff
   $scope.authentication_reset = function () {
     $scope.authentication_full_name = $scope.authentication_email = $scope.authentication_password = $scope.authentication_confirm_password = '';
@@ -15,12 +24,10 @@ function mainController($rootScope, $window, $scope) {
   }
   $scope.authentication_error_reset();
   
-  // Our variables
   $scope.authentication_state = 'signing_in';
   $scope.user = null;
   $scope.terms_read = false;
   $scope.authentication_in_progress = false;
-  
   
   $scope.authentication_state_change = function (s) {
     $scope.authentication_reset();
@@ -51,16 +58,28 @@ function mainController($rootScope, $window, $scope) {
     }
     else if ($scope.authentication_state == 'signing_in') {
       firebase.auth().signInWithEmailAndPassword($scope.authentication_email, $scope.authentication_password).catch(function(error) {
-          if (error.code == 'auth/wrong-password') {
-            $scope.authentication_error_password = error.message;
-          }
-          else {
-            $scope.authentication_error_email = error.message;
-          }
-          $scope.authentication_in_progress = false;
-          $scope.$apply();
+        if (error.code == 'auth/wrong-password') {
+          $scope.authentication_error_password = error.message;
+        }
+        else {
+          $scope.authentication_error_email = error.message;
+        }
+        $scope.authentication_in_progress = false;
+        $scope.$apply();
       });
     }
+  }
+  
+  $scope.authentication_reset_password = function () {
+    firebase.auth().sendPasswordResetEmail($scope.authentication_email).then(function (r) {
+      $scope.authentication_error_email = 'An email has been sent to this address with further instructions';
+      $scope.authentication_in_progress = false;
+      $scope.$apply();
+    }, function(error) {
+      $scope.authentication_error_email = error.message;
+      $scope.authentication_in_progress = false;
+      $scope.$apply();
+    });
   }
   
   $scope.sign_out = function () {
@@ -78,6 +97,14 @@ function mainController($rootScope, $window, $scope) {
     $scope.user = user;
     
     if ($scope.user != null) {
+      // Lets get the rest of the info of the user
+      $scope.messages_reference = firebase.database().ref('/users/' + firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+        var v = snapshot.val();
+        $scope.user.name = (v != null  && v.name != undefined) ? v.name : '';
+        $scope.get_url_of_image(((v != null  && v.profile_picture != undefined) ? v.profile_picture : ''), function(r) { $scope.user.profile_picture = r });
+      });
+      
+      // Lets reset the authentication screen
       $scope.authentication_reset();
       $scope.authentication_error_reset();
       $scope.authentication_state = 'signing_in';
@@ -87,6 +114,49 @@ function mainController($rootScope, $window, $scope) {
     }
   });
 
+  
+  //Profile stuff
+  $scope.my_profile_visible = false;
+  
+  $scope.show_my_profile = function (b) {
+    $scope.my_profile_visible = b;
+  };
+  
+  $scope.editing_profile = false;
+  $scope.toggle_edit_profile = function () {
+    $scope.editing_profile = !$scope.editing_profile;
+  }
+  
+  $scope.edit_profile_confirm = function (e) {
+      if (e.code == 'Enter') {
+        $scope.editing_profile = false;
+        firebase.database().ref('/users/' + firebase.auth().currentUser.uid).update({
+            name: $scope.user.name
+          });
+      }
+  }
+  
+  $scope.select_profile_picture = function () {
+    document.getElementById('profile_picture_file').click();
+  }
+  
+  $scope.upload_profile_picture = function (e) {
+    var file = e.target.files[0];
+    if (!file.type.match('image.*'))
+      return;
+    
+    if ($scope.user != null) {
+      var uploadTask = firebase.storage().ref(firebase.auth().currentUser.uid + '/' + Date.now() + '/' + file.name).put(file, {'contentType': file.type});
+      
+      uploadTask.on('state_changed', null, function(error) {
+        console.error('There was an error uploading a file to Firebase Storage:', error);
+      }, function() {
+        var url = firebase.storage().ref(uploadTask.snapshot.metadata.fullPath).toString();
+        firebase.database().ref('/users/' + firebase.auth().currentUser.uid).update({profile_picture: url});
+        $scope.get_url_of_image(url, function(r) { $scope.user.profile_picture = r });
+      });
+    }
+  }
 
   // Chat stuff
   $scope.sending_message = false;
@@ -95,9 +165,9 @@ function mainController($rootScope, $window, $scope) {
   $scope.messages_reference = null;
 
   $scope.add_message = function(data) {
-      $scope.messages.push(data.val());
-      $scope.$apply();
-    };
+    $scope.messages.push(data.val());
+    $scope.$apply();
+  }
   
   $scope.load_messages = function () {
     $scope.messages_reference = firebase.database().ref('messages');
