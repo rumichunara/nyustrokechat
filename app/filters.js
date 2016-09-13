80e4a8d2ab49c7a9d5bca0051d27a9d1f803e7c2
Andres Barrios <andybar2@gmail.com>
2016-09-13 19:19:48 -0300

    Fix more ci issues.

--- app/filters.js
+++ app/filters.js
@@ -2,18 +2,23 @@ var date = require( '../node_modules/locutus/php/datetime/date' );
 
 
 angular
-  .module('app')
-  .filter('showTime', showTimeFilter);
+  .module( 'app' )
+  .filter( 'showTime', showTimeFilter );
   
 
 function showTimeFilter() {
-  return function(a) {
-    var d = new Date(a);
+  return function filter( a ) {
+    var d = new Date( a );
     var s = '';
-    if (date('m-d', d) != date('m-d'))
-      s += date('M j');
-    if (date('Y', d) != date('Y'))
-      s += ((s != '') ? ', ' : '') + date('Y');
-    return ((s != '') ? ' - ' : '') + date('H:i', d);
+
+    if ( date( 'm-d', d ) !== date( 'm-d' ) ) {
+      s += date( 'M j' );
+    }
+
+    if ( date( 'Y', d ) !== date( 'Y' ) ) {
+      s += ( ( s !== '' ) ? ', ' : '' ) + date( 'Y' );
+    }
+
+    return ( ( s !== '' ) ? ' - ' : '' ) + date( 'H:i', d );
   };
 }
